cf889ade1eaff575ed00bdeaca046afdfeb4c84f
bongsong kim <43867065+bongsongkim@users.noreply.github.com>
2019-08-20 14:07:49 -0500

    Update Figure_1.R

--- Figure_1.R
+++ Figure_1.R
@@ -1,10 +1,7 @@
                                           
-   install.packages("RCurl")                  
-   require(RCurl) 
-
 # Figure 1(A) Correlation between Naive-BLUP and K-BLUP   
 
-   dat1 <- read.csv(text=getURL("https://raw.githubusercontent.com/bongsongkim/BLUP/master/raw_data/Naive-BLUP_estimates.csv"),header=T)
-   dat2 <- read.csv(text=getURL("https://raw.githubusercontent.com/bongsongkim/BLUP/master/raw_data/K-BLUP_estimates.csv"),header=T)   
+   dat1 <- read.csv("https://raw.githubusercontent.com/bongsongkim/BLUP/master/raw_data/Naive-BLUP_estimates.csv",header=T)
+   dat2 <- read.csv("https://raw.githubusercontent.com/bongsongkim/BLUP/master/raw_data/K-BLUP_estimates.csv",header=T)   
    plot(dat1[,2],dat2[,2],xlab="Naive-BLUP",ylab="K-BLUP")
    
