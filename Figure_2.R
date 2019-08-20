07710d6f1fbadb9c205187ba1e32710fa1b08e6b
bongsong kim <43867065+bongsongkim@users.noreply.github.com>
2019-08-20 07:40:09 -0500

    Update Figure_2.R

--- Figure_2.R
+++ Figure_2.R
@@ -1,6 +1,6 @@
 
-  bv         <- read.csv(text=getURL("https://raw.githubusercontent.com/bongsongkim/BLUP/master/raw_data/K-BLUP_estimates.csv"),header=T)      
-  k_blup.kin <- read.csv(text=getURL("https://raw.githubusercontent.com/bongsongkim/BLUP/master/raw_data/genomic_kinship_matrix.CSV"),row.names=1)
+  bv    <- read.csv(text=getURL("https://raw.githubusercontent.com/bongsongkim/BLUP/master/raw_data/K-BLUP_estimates.csv"),header=T)      
+  g.kin <- read.csv(text=getURL("https://raw.githubusercontent.com/bongsongkim/BLUP/master/raw_data/genomic_kinship_matrix.CSV"),row.names=1)
 
   z <- read.csv(text=getURL("https://raw.githubusercontent.com/bongsongkim/BLUP/master/raw_data/z_matrix.csv"),header=T)
   z <- as.matrix(z)                      # Z matrix 
@@ -8,14 +8,13 @@
   y  <- as.matrix(read.csv("./Processed_data/y.csv",header=F))   # phenotypic vector
   m.y <- as.matrix(rep(mean(y),dim(y)[1]))                       # a vector containing the phenotypic averages
 
-  kk  <- (ginv(t(z) %*% z) %*% t(z) %*% (y - m.y) %*% t(y - m.y) %*% (z %*% ginv(t(z) %*% z))) / dim(ginv(t(z) %*% z) %*% t(z) %*% (y - m.y))[1]
+  naive_var_cov <- (ginv(t(z) %*% z) %*% t(z) %*% (y - m.y) %*% t(y - m.y) %*% (z %*% ginv(t(z) %*% z))) / dim(ginv(t(z) %*% z) %*% t(z) %*% (y - m.y))[1]
    # kk = a kinship matrix of the Naive-BLUP
 
 #  var_cov <- read.csv(text=getURL("https://raw.githubusercontent.com/bongsongkim/BLUP/master/raw_data/variance-covariance.csv"),row.names=1)
 
   g.kin <- g.kin[match(bv[,1],rownames(g.kin)),match(bv[,1],rownames(g.kin))] 
-  colnames(p.kin) <- colnames(g.kin)
-  rownames(p.kin) <- rownames(g.kin)
+  k_blup_var_cov <- 1.076 * g.kin  
 
   cnt   <- 0
   vec_1 <- NULL
@@ -23,7 +22,7 @@
   for (i in 1:107){
     for (j in i:107){
        cnt        <- cnt + 1
-       vec_1[cnt] <- g.kin[i,j]
-       vec_2[cnt] <- p.kin[i,j]
+       vec_1[cnt] <- naive_var_cov[i,j]
+       vec_2[cnt] <- k_blup_var_cov[i,j]
   }}
   plot(vec_2,vec_1,xlab="Var(u) calculated by Equation 7",ylab="Var(u) calculated by λK")
