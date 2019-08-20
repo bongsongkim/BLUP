37061e35acdd5bf89c389a29972abf2c55e617d3
bongsong kim <43867065+bongsongkim@users.noreply.github.com>
2019-08-20 07:41:25 -0500

    Update Figure_2.R

--- Figure_2.R
+++ Figure_2.R
@@ -25,4 +25,4 @@
        vec_1[cnt] <- naive_var_cov[i,j]
        vec_2[cnt] <- k_blup_var_cov[i,j]
   }}
-  plot(vec_1,vec_2,xlab="Var(u) calculated by Equation 7",ylab="Var(u) calculated by λK")
+  plot(vec_1,vec_2,xlab="Var(u) calculated by Equation 7",ylab="Var(u) calculated by λK (λ = 1.076)")
