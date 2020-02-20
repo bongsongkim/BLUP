cb9390d5352d9344ca9a47e9ae17a498cf2467b5
bongsong kim <43867065+bongsongkim@users.noreply.github.com>
2020-02-20 00:11:31 -0600

    Update Figure_1.R

--- Figure_1.R
+++ Figure_1.R
@@ -3,5 +3,5 @@
 
    dat1 <- read.csv("https://raw.githubusercontent.com/bongsongkim/BLUP/master/raw_data/Naive-BLUP_estimates.csv",header=T)
    dat2 <- read.csv("https://raw.githubusercontent.com/bongsongkim/BLUP/master/raw_data/K-BLUP_estimates.csv",header=T)   
-   plot(dat1[,2],dat2[,2],xlab="Naive-BLUP",ylab="K-BLUP")
+   plot(dat1[,2],dat2[,2],xlab="Naive-BLUP",ylab="Conventional BLUP")
    
